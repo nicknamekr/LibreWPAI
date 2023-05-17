9ad182d6b16c73a58b2abb77335d00eeb891df7a
Nickname <47962021+nicknamekr@users.noreply.github.com>
2023-05-17 16:06:28 +0900

    Update setting.py

--- setting.py
+++ setting.py
@@ -20,7 +20,7 @@ Topic = '' # 주제를 설정하세요.
 Topic_English = translator.translate(Topic).text # Unsplash용 영문 주제 (기본값 : 자동번역)
 category = [Topic] # 카테고리를 설정하세요. (기본 값 : Topic)
 
-OpenAI_Key = '' # OpenAI 키를 입력하세요
+OpenAI_Key = 'pk-FcRodAetTQBBbNwvAWlKoHYdRxvGeFyHBZHARdQskUlBOxiX' # OpenAI 키를 입력하세요 (For now, we supports freegpt key)
 
 txtSaveMode = True # TXT 파일 세이브를 할 지 선택합니다. (기본값 "True")
 txtSaveDirectory = '/' # 슬래쉬 또는 역슬래쉬를 정합니다. Mac, Linux는 슬래쉬(/)이고 Windows는 역슬래쉬(\)입니다. (기본값 '/')
